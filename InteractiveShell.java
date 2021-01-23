088cbfd00e0e30df761e649129d7aa35dfd23314
Johnny Wan <wonjohnny@outlook.com>
2021-01-23 13:51:31 -0800

    started interactive shell, gonna switch to storage module first.

--- InteractiveShell.java
+++ InteractiveShell.java
@@ -7,4 +7,33 @@ public class InteractiveShell {
             instance = this;
         }
     }
+
+    public void Start() { // pass control to shell
+        String newCommand = "";
+        Boolean interrupted = false;
+        System.out.println("PigeonStack: Entering interactive shell");
+
+        // main routine
+        while (!interrupted) {
+            System.out.printf(">> ");
+            newCommand = System.console().readLine();
+            switch (newCommand) {
+                case "reset":
+                    this.Reset();
+                    break;
+                default:
+                    System.out.println("Error: invalid command");
+                    break;
+            }
+        }
+    }
+
+    public void Reset() { // reset stored arguments
+
+    }
+
+    public static void main(String args[]) { // debug code
+        new InteractiveShell();
+        InteractiveShell.instance.Start();
+    }
 }
