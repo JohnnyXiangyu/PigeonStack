5647d2ffeba11070c26395fb85f47c0098e44e91
Johnny Wan <wonjohnny@outlook.com>
2021-01-23 17:20:25 -0800

    implement global setting related methods

--- JsonStorageModule.java
+++ JsonStorageModule.java
@@ -1,7 +1,10 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.json.simple.JSONArray;
@@ -15,27 +18,64 @@ public class JsonStorageModule implements StorageModule {
         public LinkedList<HashMap<String, String>> listNames = new LinkedList<HashMap<String, String>>();
         public String pathToFiles = "";
 
-        public Settings() throws IOException {
+        public Settings() {
             // load json file
-            Path globalSettingPath = Path.of("storage_module_settings/json/global.json");
-            String globalSettingStr = Files.readString(globalSettingPath);
-            JSONObject globalSetting = (JSONObject) JSONValue.parse(globalSettingStr);
+            try {
+                Path globalSettingPath = Path.of("storage_module_settings/json/global.json");
+                String globalSettingStr = Files.readString(globalSettingPath);
+                JSONObject globalSetting = (JSONObject) JSONValue.parse(globalSettingStr);
+                
+                // get names of lists
+                JSONArray listNameArr = (JSONArray) globalSetting.get("lists");
+                for (int i = 0; i < listNameArr.size(); i++) {
+                    JSONObject tempListMeta = (JSONObject) listNameArr.get(i);
+                    HashMap<String, String> newDict = new HashMap<String, String>();
 
-            // get names of lists
-            JSONArray listNameArr = (JSONArray) globalSetting.get("lists");
-            for (int i = 0; i < listNameArr.size(); i++) {
-                JSONObject tempListMeta = (JSONObject) listNameArr.get(i);
-                HashMap<String, String> newDict = new HashMap<String, String>();
+                    // metainfo of each list
+                    newDict.put("name", (String) tempListMeta.get("name"));
+                    newDict.put("alias", (String) tempListMeta.get("alias"));
 
-                // metainfo of each list
-                newDict.put("name", (String) tempListMeta.get("name"));
-                newDict.put("alias", (String) tempListMeta.get("alias"));
+                    listNames.add(newDict);
+                }
 
-                listNames.add(newDict);
+                // get path to files
+                pathToFiles = (String) globalSetting.get("path");
+            } catch (IOException e) {
+                // file can't be opened
+
+                // prompt for new setting
+                System.out.println("Json Storage Module: no global config found, create new? Y/n");
+                
+                String response = System.console().readLine();
+                if (response == "n" || response == "N") {
+                    System.out.println("Json Storage Module: config creation cancelled, exiting ...");
+                }
+                else {
+                    // get path to file
+                    Boolean success = false;
+                    System.out.println("Json Storage Module: input path to lists' folder");
+                    while (!success) {
+                        response = System.console().readLine();
+                        if (response == "!e") {
+                            // user breaks
+                            System.out.println("Json Storage Module: operation cancelled, exiting ...");
+                            System.exit(0);
+                        }
+                        try {
+                            File myObj = new File(response + (response.charAt(response.length()-1) == '/' ? "badtoken" : "/badtoken"));
+                            myObj.createNewFile(); // duplicated or not, I just care about if there's a file to write
+                            success = true;
+                        } catch (IOException ne) {
+                            success = false;
+                            System.out.println("Json Storage Module: path invalid, please try again (enter \"!e\" to cancel");
+                        }
+                    }
+
+                    // output settings
+                    OutputGlobalConfig();
+                }
             }
 
-            // get path to files
-            pathToFiles = (String) globalSetting.get("path");
         }
     }
 
@@ -51,23 +91,54 @@ public class JsonStorageModule implements StorageModule {
 
         // initial setting loading
         if (this.mySettings == null) {
-            try {
-                this.mySettings = new Settings();
-            } catch (IOException e) {
-                System.out.println("JSON Storge Module: no global config found ... creating new config");
-                // TODO: when global config file can't be loaded, create a new file
-            }
+            this.mySettings = new Settings();
         }
     }
 
-    private void Reset() { // create a new object replacing this
-        instance = null;
-        new JsonStorageModule();
-    }
+    // private void Reset() { // create a new object replacing this
+    //     instance = null;
+    //     new JsonStorageModule();
+    // }
 
     private void OutputGlobalConfig() { // serialize current settings to a new file
-        // TODO: serialze everything to a json string
-        // TODO: write json string to storage_module_settings/json/global.json
+        JSONObject fileToWrite = new JSONObject();
+        JSONArray listArr = new JSONArray();
+
+        // serialize json array
+        Iterator<HashMap<String, String>> it = mySettings.listNames.iterator();
+        while (it.hasNext()) {
+            HashMap<String, String> temp = it.next();
+            JSONObject tempJson = new JSONObject();
+            tempJson.put("name", (String) temp.get("name"));
+            tempJson.put("alias", (String) temp.get("alias"));
+
+            listArr.add(tempJson);
+        }
+
+        // put together
+        fileToWrite.put("path", this.mySettings.pathToFiles);
+        fileToWrite.put("lists", listArr);
+
+        // try creating a file
+        try {
+            File myObj = new File("storage_module_settings/json/global.json");
+            myObj.createNewFile(); // duplicated or not, I just care about if there's a file to write
+        } catch (IOException e) {
+            System.out.println("Json Storage Module: can't create config file");
+            System.out.println(e.getMessage());
+            System.exit(200);
+        }
+
+        // write to file
+        try {
+            FileWriter myWriter = new FileWriter("storage_module_settings/json/global.json");
+            myWriter.write(fileToWrite.toString());
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("Json Storage Module: can't write to config file");
+            System.out.println(e.getMessage());
+            System.exit(220);
+        }
     }
 
     // interface functions
